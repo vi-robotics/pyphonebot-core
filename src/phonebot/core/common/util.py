792378e5b730bd65686648543b5449427bea04f5
Max Schommer <maxschommer@gmail.com>
2021-07-28 00:48:08 -0700

    Removed unused state.py and refactored util.py

--- src/phonebot/core/common/util.py
+++ src/phonebot/core/common/util.py
@@ -5,20 +5,10 @@ Utility Functions.
 """
 
 import time
+from typing import Any, Optional
 import numpy as np
 
 __all__ = ['find_nearest_index', 'find_k_nearest_indices']
-
-
-# phonebot_time = 0.0
-# def increment_time(dt=0.1):
-#    global phonebot_time
-#    phonebot_time += dt
-#
-#
-# def get_time():
-#    global phonebot_time
-#    return phonebot_time
 
 
 time_scale = 0.5
@@ -36,20 +26,40 @@ def get_time_scale():
     return time_scale
 
 
-def find_nearest_index(array, value):
+def find_nearest_index(array: np.ndarray, value: Any) -> int:
     """
     Find the index of the element nearest to value in an array.
+
     https://stackoverflow.com/a/26026189
+
+
+    Args:
+        array (np.ndarray): The array to query
+        value (Any): The value to search for in the array.
+
+    Returns:
+        int: The index of the nearest value.
     """
     idx = np.searchsorted(array, value, side="left")
     is_left = idx > 0 and (idx == len(array) or np.abs(
-        value - array[idx-1]) < np.abs(value - array[idx]))
+        value - array[idx - 1]) < np.abs(value - array[idx]))
     return idx - is_left
 
 
-def find_k_nearest_indices(array, value, k=1):
-    """
-    Find k nearest indices in an array, sorted by proximity.
+def find_k_nearest_indices(array: np.ndarray,
+                           value: Any,
+                           k: int = 1) -> Optional[np.ndarray]:
+    """Find k nearest indices in an array, sorted by proximity.
+
+    Args:
+        array (np.ndarray): The array to find nearest indices in.
+        value (Any): The value in the array to find the nearest indices
+            to.
+        k (int, optional): The number of indices to return. Defaults to 1.
+
+    Returns:
+        Optional[np.ndarray]: An #K vector of indices, or None if there are
+            fewer than k elements in the array.
     """
     n = len(array)
     if n < k:
@@ -69,4 +79,4 @@ def find_k_nearest_indices(array, value, k=1):
         else:
             indices.append(nxt_index)
             nxt_index += 1
-    return indices
+    return np.asarray(indices)
